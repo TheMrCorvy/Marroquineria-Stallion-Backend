ce976121286d785fd377e6e5ad6cdc2acee0dc23
gonzalo salvador corvalan <mr.corvy@gmail.com>
2021-07-20 08:40:53 -0300

    added the basic styles

---
+++ resources/views/layout/head.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="apple-touch-icon" sizes="76x76" href="{{asset('argon')}}/img/apple-icon.png">
+  <title>
+    PasuNashi Dashboard
+  </title>
+  <!--     Fonts and icons     -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
+  <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
+  <!-- Nucleo Icons -->
+  <link href="{{asset('argon')}}/css/nucleo-icons.css" rel="stylesheet" />
+  <link href="{{asset('argon')}}/css/nucleo-svg.css" rel="stylesheet" />
+  <!-- Font Awesome Icons -->
+  <link href="{{asset('argon')}}/css/font-awesome.css" rel="stylesheet" />
+  <link href="{{asset('argon')}}/css/nucleo-svg.css" rel="stylesheet" />
+  <!-- CSS Files -->
+  <link href="{{asset('argon')}}/css/argon-design-system.css?v=1.0.2" rel="stylesheet" />
+</head>
+    

4d15ca9e33d8ca7ec6e14eb171bc63546ae9abba
gonzalo salvador corvalan <mr.corvy@gmail.com>
2021-07-22 15:46:13 -0300

    added padding

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,5 +1,5 @@
 @extends('layout.app', ['class' => 'bg-secondary']) @section('content') @include('layout.navbar')
-<div class="container-fluid mt-6">
+<div class="container-fluid mt-6 pb-6">
 	@if (Session::has('message'))
 		<div class="alert alert-success my-5" role="alert">
 			<strong>Éxito!</strong> {{Session::get('message')}}

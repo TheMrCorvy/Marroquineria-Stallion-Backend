cee897d613bd78fb056cc3e4ee833252b4f15fa0
gonzalo salvador corvalan <mr.corvy@gmail.com>
2021-07-26 12:03:38 -0300

    small changes and edits

--- app/Http/Controllers/SaleController.php
+++ app/Http/Controllers/SaleController.php
@@ -65,6 +65,7 @@ class SaleController extends Controller
     public function buy(Request $request)
     {
         $info = $request->only(
+            'method',
             'billing_info',
             'shipping_info',
             'total_price',
@@ -72,14 +73,13 @@ class SaleController extends Controller
             'email',
             'card_network',
             'card_token',
-            'shipping_option_id',
-            'method'
+            'shipping_option_id'
         );
 
         $validator = Validator::make(
             $info,
             [
-                'method' => ['required', 'string', 'min:3', 'max:15'],
+                'method' => ['required', 'string', 'min:3', 'max:15', 'in:mercadopago,cash'],
                 'billing_info' => ['required', 'json'],
                 'shipping_info' => ['required', 'json'],
                 'total_price' => ['required', 'integer', 'min:1'],
@@ -98,15 +98,6 @@ class SaleController extends Controller
             ], 400);
         }
 
-        if ($info['method'] !== 'mercadopago' && $info['method'] !== 'cash') {
-            return response()->json([
-                'errors' => [
-                    'payment_method' => 'invalid payment method'
-                ],
-                'message' => 'Método de pago incorrecto.'
-            ], 400);
-        }
-
         $products_exist_in_db = $this->verify_products($info['cart_items']);
 
         if (!$products_exist_in_db) {
@@ -118,6 +109,19 @@ class SaleController extends Controller
             ], 400);
         }
 
+        try {
+            $shipping_option = ShippingZone::findOrFail($info['shipping_option_id']);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'errors' => [
+                    'db_connection_unsuccessful' => 'there was an error trying to get the shipping option price'
+                ],
+                'message' => 'Hubo un error calculando el precio de la modalidad de envío elegida. Por favor intente otra vez.'
+            ], 500);
+        }
+
+        $total_amount = $info['total_price'] + $shipping_option->actual_price;
+
         $payment_method = 'Efectivo en el Local';
 
         if ($info['method'] === 'mercadopago') {
@@ -125,11 +129,10 @@ class SaleController extends Controller
                 $info['card_network'],
                 $info['card_token'],
                 $info['email'],
-                $info['total_price'],
-                $info['shipping_id']
+                $total_amount,
             );
 
-            if ($payment->status !== 'approved') {
+            if ($payment[0]->status !== 'approved') {
                 return response()->json([
                     'errors' => [
                         'api_call_unsuccessful' => 'the api responded with a status different from "approved"'
@@ -144,7 +147,7 @@ class SaleController extends Controller
         $sale_order = SaleOrder::create([
             'date' => now()->format('Y-M-D'),
             'payment_method' => $payment_method,
-            'total_price' => $info['total_price'], // más el envio... calcular luego
+            'total_price' => $total_amount,
             'billing_info' => $info['billing_info'],
             'shipping_info' => $info['shipping_info'],
         ]);
@@ -156,7 +159,7 @@ class SaleController extends Controller
                 'errors' => [
                     'db_connection_unsuccessful' => 'there was an error trying to reduce the stock of some products, or trying to create the sale order'
                 ],
-                'message' => 'Su orden de compra no pudo ser finalizada, pero su pago fue procesado. Le recomendamos que nos contactes para poder ayudarte.'
+                'message' => 'Su orden de compra no pudo ser finalizada, pero su pago fue procesado. Le recomendamos que nos contacte para poder ayudarle.'
             ], 500);
         }
 
@@ -180,10 +183,10 @@ class SaleController extends Controller
         return true;
     }
 
-    private function call_mercadopago($card_network, $card_token, $email, $total_amount, $shipping_id)
+    private function call_mercadopago($card_network, $card_token, $email, $total_amount)
     {
         try {
-            $shipping_option = ShippingZone::findOrFail($shipping_id);
+            //
         } catch (\Throwable $th) {
             return false;
         }
